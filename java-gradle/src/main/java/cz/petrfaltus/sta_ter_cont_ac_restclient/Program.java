5b5b592ff70dfb5584927b6b0593a2342c08dbd8
Petr Faltus <petrfaltus@petrfaltus.net>
2021-01-23 21:41:35 +0100

    Java/Gradle - create the empty console application

---
+++ java-gradle/src/main/java/cz/petrfaltus/sta_ter_cont_ac_restclient/Program.java
@@ -0,0 +1,11 @@
+package cz.petrfaltus.sta_ter_cont_ac_restclient;
+
+import static java.lang.System.out;
+
+public class Program {
+
+	public static void main(String[] args) {
+		out.println("YES");
+	}
+
+}
