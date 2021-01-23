0a6f866d5e8cce87761285a508f41603f51a5895
Petr Faltus <petrfaltus@petrfaltus.net>
2021-01-23 18:25:49 +0100

    Java/Maven - implement the web POST request for searching

---
+++ java-maven/src/main/java/cz/petrfaltus/sta_ter_cont_ac_restclient/Const.java
@@ -0,0 +1,8 @@
+package cz.petrfaltus.sta_ter_cont_ac_restclient;
+
+public class Const {
+    public static final String URL_ADDRESS = "http://api.petrfaltus.net/country_code_prefix/json/1.0";
+    public static final String CHARSET = "UTF-8";
+
+    public static final String EMPTY_STRING = "";
+}
