0075fadfd0d9bbebacb3278bd20a9437b3fa8dd1
Petr Faltus <petrfaltus@petrfaltus.net>
2021-01-23 21:42:36 +0100

    Java/Gradle - implement the web POST request for searching

--- java-gradle/src/main/java/cz/petrfaltus/sta_ter_cont_ac_restclient/Program.java
+++ java-gradle/src/main/java/cz/petrfaltus/sta_ter_cont_ac_restclient/Program.java
@@ -5,7 +5,10 @@ import static java.lang.System.out;
 public class Program {
 
 	public static void main(String[] args) {
-		out.println("YES");
+		String requestJson = "{ \"method_number\":3, \"owner\":\"francie\", \"continent\":\"tichý oceán\" }";
+
+		String replyJson = Web.request(requestJson);
+		out.println(replyJson);
 	}
 
 }
