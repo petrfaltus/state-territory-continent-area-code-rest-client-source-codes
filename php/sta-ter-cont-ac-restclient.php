7c5cfe901574bfede20fdfec3f48158943c88bb6
Petr Faltus <petrfaltus@petrfaltus.net>
2021-01-24 19:06:55 +0100

    PHP - implement the web POST request for searching

--- php/sta-ter-cont-ac-restclient.php
+++ php/sta-ter-cont-ac-restclient.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "Yes".PHP_EOL;
+// constants
+const PATH_TO_CLASSES = ".class";
+
+// user classes autoloading
+spl_autoload_register(function($class_name)
+{
+  $class_source_file_path = PATH_TO_CLASSES."/".$class_name.".php";
+  require_once $class_source_file_path;
+});
+
+$requestJson = "{ \"method_number\":3, \"owner\":\"francie\", \"continent\":\"tichý oceán\" }";
+$replyJson = tWeb::request($requestJson);
+echo $replyJson.PHP_EOL;
 
 ?>
