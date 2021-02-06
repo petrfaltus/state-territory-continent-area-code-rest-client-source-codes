aaf4b1f224095044f4ae5136daa7e3d3330fb041
Petr Faltus <petrfaltus@petrfaltus.net>
2021-02-06 20:41:13 +0100

    C# - separate the web client code to new class

--- csharp/01-compile.cmd
+++ csharp/01-compile.cmd
@@ -11,4 +11,4 @@ set DOTNET_HOME=C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319
 set DLL=%OUT%\Newtonsoft.Json.dll
 
 mkdir %OUT%
-"%DOTNET_HOME%\csc.exe" /win32icon:%ICON% /out:%EXECUTABLE% /reference:%DLL% %SOURCE%.cs OneItem.cs RestRequest1.cs RestRequest2.cs RestRequest3.cs RestReply1or2.cs RestReply3.cs
+"%DOTNET_HOME%\csc.exe" /win32icon:%ICON% /out:%EXECUTABLE% /reference:%DLL% %SOURCE%.cs Web.cs OneItem.cs RestRequest1.cs RestRequest2.cs RestRequest3.cs RestReply1or2.cs RestReply3.cs
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Text;
 
 using Newtonsoft.Json;
 
@@ -9,19 +7,11 @@ namespace StaTerContAcRestClient
 {
     public class Program
     {
-        private static readonly string URL_ADDRESS = "http://api.petrfaltus.net/country_code_prefix/json/1.0";
-        private static readonly Encoding encoding = Encoding.UTF8;
-        private static readonly string USER_AGENT = "Petr Faltus C# State, territory, continent and area code REST client";
-
         private static readonly string MESSAGE_ERROR_CONTACTING_SERVICE = "error contacting the REST service";
         private static readonly string MESSAGE_RECEIVED_ERROR = "received error";
 
         public static void Main(string[] args)
         {
-            WebClient client = new WebClient();
-            client.Encoding = encoding;
-            client.Headers.Add("user-agent", USER_AGENT);
-
             // all continents query
             Console.WriteLine("All continents:");
 
@@ -32,7 +22,7 @@ namespace StaTerContAcRestClient
             string restReplyJsonContinents;
             try
             {
-                restReplyJsonContinents = client.UploadString(URL_ADDRESS, restRequestJsonContinents);
+                restReplyJsonContinents = Web.Request(restRequestJsonContinents);
             }
             catch (Exception)
             {
@@ -71,7 +61,7 @@ namespace StaTerContAcRestClient
             string restReplyJson;
             try
             {
-                restReplyJson = client.UploadString(URL_ADDRESS, restRequestJson);
+                restReplyJson = Web.Request(restRequestJson);
             }
             catch (Exception)
             {
