ba08c26dfdfb101425f75acc863a91c60bbdc63a
Petr Faltus <petrfaltus@petrfaltus.net>
2021-03-31 15:20:02 +0200

    Python - implement the JSON encoding and decoding for other two REST requests

--- python/StaTerContAcRestClient.py
+++ python/StaTerContAcRestClient.py
@@ -30,3 +30,48 @@ if continents == None:
 for continent in continents:
     countries = str(continents[continent])
     print(" - " + continent + " (" + countries + " countries}")
+
+print()
+
+# one query
+print("One query:")
+
+country = ""
+iso_code_2_char = ""
+iso_code_3_char = ""
+owner = "francie"
+continent = "tichý oceán"
+phone_prefix = ""
+
+requestJson = Json.codeQueryOneQuery(country, iso_code_2_char, iso_code_3_char, owner, continent, phone_prefix)
+
+try:
+    replyJson = Web.request(requestJson)
+except:
+    print(" - " + MESSAGE_ERROR_CONTACTING_SERVICE)
+    exit()
+
+replyItems = Json.decodeResult(replyJson)
+if replyItems == None:
+    errorString = Json.getLastErrorString()
+
+    if errorString != None:
+        print(" - " + MESSAGE_RECEIVED_ERROR + ": " + errorString)
+    else:
+        print(" - " + MESSAGE_ERROR_DECODING_JSON)
+
+    exit()
+
+totalItems = 0
+for replyItem in replyItems:
+    if totalItems > 0:
+        print()
+
+    print(" - country: " + replyItem[Json.COUNTRY])
+    print(" - ISO code 2 characters: " + replyItem[Json.ISO_CODE_2_CHAR])
+    print(" - ISO code 3 characters: " + replyItem[Json.ISO_CODE_3_CHAR])
+    print(" - owner: " + replyItem[Json.OWNER])
+    print(" - continent: " + replyItem[Json.CONTINENT])
+    print(" - phone prefix: " + replyItem[Json.PHONE_PREFIX])
+
+    totalItems = totalItems + 1
--- python/classes/Json.py
+++ python/classes/Json.py
@@ -2,18 +2,46 @@ import json
 
 class Json:
     METHOD_NUMBER = "method_number"
+    COUNTRY = "country"
+    ISO_CODE_2_CHAR = "iso_code_2_char"
+    ISO_CODE_3_CHAR = "iso_code_3_char"
+    OWNER = "owner"
+    CONTINENT = "continent"
+    PHONE_PREFIX = "phone_prefix"
 
     ERROR_CODE = "error_code"
     ERROR_STRING = "error_string"
     DATA = "data"
 
+    METHOD_OWNERS_NUMBER = 1
     METHOD_CONTINENTS_NUMBER = 2
+    METHOD_ONE_QUERY_NUMBER = 3
 
     lastErrorString = None
 
     @staticmethod
+    def codeQueryOwners():
+        output = {Json.METHOD_NUMBER: Json.METHOD_OWNERS_NUMBER}
+        outputJson = json.dumps(output)
+
+        return outputJson
+
+    @staticmethod
     def codeQueryContinents():
         output = {Json.METHOD_NUMBER: Json.METHOD_CONTINENTS_NUMBER}
+        outputJson = json.dumps(output)
+
+        return outputJson
+
+    @staticmethod
+    def codeQueryOneQuery(country, iso_code_2_char, iso_code_3_char, owner, continent, phone_prefix):
+        output = {Json.METHOD_NUMBER: Json.METHOD_ONE_QUERY_NUMBER,
+                  Json.COUNTRY: country,
+                  Json.ISO_CODE_2_CHAR: iso_code_2_char,
+                  Json.ISO_CODE_3_CHAR: iso_code_3_char,
+                  Json.OWNER: owner,
+                  Json.CONTINENT: continent,
+                  Json.PHONE_PREFIX: phone_prefix}
         outputJson = json.dumps(output)
 
         return outputJson
